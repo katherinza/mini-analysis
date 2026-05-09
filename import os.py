90ed814f4076e0b00a458eccefe9e05f2656ed65
katherinza <llevotre@gmail.com>
2026-05-09 13:27:04 +0400

    !git commit -m "Add homework 1: SQL analysis"

--- import os.py
+++
@@ -1,8 +0,0 @@
-import os
-
-DB_FILE = "tgu_practice_db.db"
-
-if os.path.exists(DB_FILE):
-    print(f"База данных найдена: {DB_FILE}  ({os.path.getsize(DB_FILE)//1024} KB)")
-else:
-    print(f"БД не найдена: {DB_FILE}")
